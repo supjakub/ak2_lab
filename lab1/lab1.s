770483679e21b742d8df46bc78100ca8f1d0d0a7
Jakub Superczynski <jakubsuper1@gmail.com>
2021-03-15 17:58:59 +0100

    Pobieranie danych z klawiatury

--- lab1/lab1.s
+++ lab1/lab1.s
@@ -2,7 +2,7 @@
 
 .data
     message: .ascii "Podaj zdanie:\n"
-    message_length = . - message
+    buffer: .space 100    
 
 .text
 
@@ -10,8 +10,20 @@
         mov $4, %eax
         mov $1, %ebx
         mov $message, %ecx
-        mov $message_length, %edx
+        mov $14, %edx
         int $0x80
+
+        mov $3, %eax
+        mov $0, %ebx
+        mov $buffer, %ecx
+        mov $100, %edx
+        int $0x80
+
+        mov $4, %eax
+        mov $1, %ebx
+        mov $buffer, %ecx
+        mov $100, %edx
+        int $0x80      
 
         mov $1, %eax
         mov $0, %ebx
