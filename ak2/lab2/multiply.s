294543871915875f6bdf898d39ba0b3faa3485fb
Jakub Superczynski <jakubsuper1@gmail.com>
2021-04-23 22:52:24 +0200

    Usuwanie zer

--- ak2/lab2/multiply.s
+++ ak2/lab2/multiply.s
@@ -39,25 +39,28 @@ _start:
     lea esi, [ascii1]
     lea edi, [number1]
     call ascii_to_hex
+    lea esi, [ascii2]
+    lea edi, [number2]
+    call ascii_to_hex
 
     ;Wyjscie z programu
     mov eax, 1
     int 80h
 
 ascii_to_hex:
-    next_digit:
-    movzx eax, byte[esi]
-    inc esi
+    next_byte:
+    mov al, byte [esi]
     cmp al, 10
-    je end_loop
+    je end
+    inc cx
     cmp al, 48
     jl bad_input
     cmp al, 57
     jg upper_case
     ;zamiana cyfr
-    sub al, 30h
-    mov [edi], al
-    inc edi
+    sub al, 30h    
+    shl al, 4
+    inc esi
     jmp next_digit
     upper_case:
     cmp al, 65
@@ -66,8 +69,8 @@ ascii_to_hex:
     jg lower_case
     ;zamiana A-F
     sub al, 37h
-    mov [edi], al
-    inc edi
+    shl al, 4
+    inc esi
     jmp next_digit
     lower_case:
     cmp al, 97h
@@ -76,11 +79,53 @@ ascii_to_hex:
     jg bad_input
     ;zamiana a-f
     sub al, 57h
+    shl al, 4
+    inc esi
+
+    next_digit:
+    mov bl, byte [esi]
+    cmp bl, 10
+    jne hex_to_ascii_continue
+    mov [edi], al
+    jmp end
+    hex_to_ascii_continue:
+    cmp bl, 48
+    jl bad_input
+    cmp bl, 57
+    jg upper_case2
+    ;zamiana cyfr
+    sub bl, 30h    
+    add al, bl
     mov [edi], al
     inc edi
-    jmp next_digit
+    inc esi
+    jmp next_byte
+    upper_case2:
+    cmp bl, 65
+    jl bad_input
+    cmp bl, 70
+    jg lower_case2
+    ;zamiana A-F
+    sub bl, 37h
+    add al, bl
+    mov [edi], al
+    inc edi
+    inc esi
+    jmp next_byte
+    lower_case2:
+    cmp bl, 97h
+    jl bad_input
+    cmp bl, 102
+    jg bad_input
+    ;zamiana a-f
+    sub bl, 57h
+    add al, bl
+    mov [edi], al
+    inc edi
+    inc esi
+    jmp next_byte
 
-    end_loop:
+    end:
     ret
 
     bad_input:
