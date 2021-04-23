11c3aeb570b95d3ce94bb9d9447f2b2e22b93b9e
Jakub Superczynski <jakubsuper1@gmail.com>
2021-04-23 17:25:57 +0200

    Konwersja, uproszczony kod

--- ak2/lab2/multiply.s
+++ ak2/lab2/multiply.s
@@ -1,130 +1,95 @@
 global _start
 
 section .data
-    
-    message1 db "Podaj mnozna: "
-    message2 db "Podaj mnoznik: "
-    message3 db "Wynik: "
-    message4 db "Nieprawidlowe dane"
-
-    multiplicand times 200 db 0
-    multiplier   times 200 db 0
+    msg1 db "Podaj mnozna: "
+    ascii1 times 200 db 0
+    msg2 db "Podaj mnoznik: "
+    ascii2 times 200 db 0
+    number1 times 200 db 0
+    number2 times 200 db 0
+    msg_bad_input db "Nieprawidlowe dane", 0xa
 
 section .text
-_start:
 
-    ; Wczytywanie danych
+_start:
+    ;Wczytywanie pierwszej liczby
     mov eax, 4
     mov ebx, 1
-    mov ecx, message1
+    mov ecx, msg1
     mov edx, 14
     int 80h
-
     mov eax, 3
     mov ebx, 1
-    mov ecx, multiplicand
+    mov ecx, ascii1
     mov edx, 200
     int 80h
 
+    ;Wczytywanie drugiej liczby
     mov eax, 4
     mov ebx, 1
-    mov ecx, message2
+    mov ecx, msg2
     mov edx, 15
     int 80h
-
     mov eax, 3
     mov ebx, 1
-    mov ecx, multiplier
+    mov ecx, ascii2
     mov edx, 200
     int 80h
 
-    ;Walidacja i konwersja mnoznej
-    mov cl, 200
-    mov eax, multiplicand
+    lea esi, [ascii1]
+    lea edi, [number1]
+    call ascii_to_hex
 
-    loop1:
-        cmp byte [eax], 32
-        jl end_loop1
-        cmp byte [eax], 48
-        jl bad_input
-        cmp byte [eax], 57
-        jg check_upper_case1
-        ; TODO: zamiana cyfr
-        jmp end_loop1
-    
-        check_upper_case1:
-        cmp byte [eax], 65
-        jl bad_input
-        cmp byte [eax], 70
-        jg check_lower_case1
-        ; TODO: zamiana A-F
-        jmp end_loop1
-
-        check_lower_case1:
-        cmp byte [eax], 97
-        jl bad_input
-        cmp byte [eax], 102
-        jg bad_input
-        ; TODO: zamiana a-f
-        jmp end_loop1
-
-        end_loop1:
-        inc eax
-        dec cl
-        cmp cl, 0
-    jne loop1
-
-    ;Walidacja i konwersja mnoznika
-    mov cl, 200
-    mov eax, multiplier
-
-    loop2:
-        cmp byte [eax], 32
-        jl end_loop2
-        cmp byte [eax], 48
-        jl bad_input
-        cmp byte [eax], 57
-        jg check_upper_case2
-        ; TODO: zamiana cyfr
-        jmp end_loop2
-    
-        check_upper_case2:
-        cmp byte [eax], 65
-        jl bad_input
-        cmp byte [eax], 70
-        jg check_lower_case2
-        ; TODO: zamiana A-F
-        jmp end_loop2
-
-        check_lower_case2:
-        cmp byte [eax], 97
-        jl bad_input
-        cmp byte [eax], 102
-        jg bad_input
-        ; TODO: zamiana a-f
-        jmp end_loop2
-
-        end_loop2:
-        inc eax
-        dec cl
-        cmp cl, 0
-    jne loop2
-    
-    ; Zakonczenie programu
+    ;Wyjscie z programu
     mov eax, 1
     int 80h
 
-    ; Zle wejscie
+ascii_to_hex:
+    next_digit:
+    movzx eax, byte[esi]
+    inc esi
+    cmp al, 10
+    je end_loop
+    cmp al, 48
+    jl bad_input
+    cmp al, 57
+    jg upper_case
+    ;zamiana cyfr
+    sub al, 30h
+    mov [edi], al
+    inc edi
+    jmp next_digit
+    upper_case:
+    cmp al, 65
+    jl bad_input
+    cmp al, 70
+    jg lower_case
+    ;zamiana A-F
+    sub al, 37h
+    mov [edi], al
+    inc edi
+    jmp next_digit
+    lower_case:
+    cmp al, 97h
+    jl bad_input
+    cmp al, 102
+    jg bad_input
+    ;zamiana a-f
+    sub al, 57h
+    mov [edi], al
+    inc edi
+    jmp next_digit
+
+    end_loop:
+    ret
+
     bad_input:
     mov eax, 4
     mov ebx, 1
-    mov ecx, message4
-    mov edx, 18
+    mov ecx, msg_bad_input
+    mov edx, 19
     int 80h
-
     mov eax, 1
     int 80h
 
-
     
-
