515b108b94d77fb33b8f9e863f59adf4aaa6921e
Jakub Superczynski <jakubsuper1@gmail.com>
2021-05-08 21:03:19 +0200

    Interfejs

--- ak2/lab3/fpu.s
+++ ak2/lab3/fpu.s
@@ -3,12 +3,17 @@ global main
 section .data
     s_format: db "%s", 10, 0
     lf_format: db "%lf", 0
+    c_format: db " %c", 0
     msg1 db "Podaj pierwszy operand: ", 0
     msg2 db "Podaj drugi operand: ", 0
+    msg3 db "Podaj dzialanie (+, -, *, /): ", 0
+    msg4 db "Podaj sposob zaokraglania: 1-nearest, 2-down, 3-up, 4-to zero: ", 0
 
 section .bss
-    number1: resb 64
-    number2: resb 64
+    number1: resq 1
+    number2: resq 1
+    operation: resb 1
+    rounding: resb 1
 
 section .text
 extern printf, scanf
@@ -29,6 +34,22 @@ main:
 
     push number2
     push lf_format
+    call scanf
+
+    push msg3
+    push s_format
+    call printf
+
+    push operation
+    push c_format
+    call scanf
+
+    push msg4
+    push s_format
+    call printf
+
+    push rounding
+    push c_format
     call scanf
 
     ;Wyjscie z programu
