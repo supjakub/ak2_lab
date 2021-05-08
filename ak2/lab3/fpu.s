5dae99cea042e577e7ce089a89018275a2d17382
Jakub Superczynski <jakubsuper1@gmail.com>
2021-05-08 03:50:53 +0200

    Pytanie o pierwszy operand

--- ak2/lab3/fpu.s
+++ ak2/lab3/fpu.s
@@ -1,12 +1,21 @@
-global _start
+global main
 
 section .data
+    msg1 db "Podaj pierwszy operand: ", 0
+    format: db "%s", 10, 0
 
 section .bss
 
 section .text
+extern printf
 
-_start:
+main:
+
+    push msg1
+    push format
+    call printf
+    add esp, 4
+    pop eax
 
     ;Wyjscie z programu
     mov eax, 1
