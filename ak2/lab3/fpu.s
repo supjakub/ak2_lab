6011e5c84ec68f9b0f5e039af1914ef62e959fad
Jakub Superczynski <jakubsuper1@gmail.com>
2021-05-08 20:29:33 +0200

    Wczytywanie liczb

--- ak2/lab3/fpu.s
+++ ak2/lab3/fpu.s
@@ -1,21 +1,35 @@
 global main
 
 section .data
+    s_format: db "%s", 10, 0
+    lf_format: db "%lf", 0
     msg1 db "Podaj pierwszy operand: ", 0
-    format: db "%s", 10, 0
+    msg2 db "Podaj drugi operand: ", 0
 
 section .bss
+    number1: resb 64
+    number2: resb 64
 
 section .text
-extern printf
+extern printf, scanf
 
 main:
 
     push msg1
-    push format
+    push s_format
     call printf
-    add esp, 4
-    pop eax
+
+    push number1
+    push lf_format
+    call scanf
+
+    push msg2
+    push s_format
+    call printf
+
+    push number2
+    push lf_format
+    call scanf
 
     ;Wyjscie z programu
     mov eax, 1
