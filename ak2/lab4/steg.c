d1223ff0d24206d74c433b9a5114e9e50ddfb759
Jakub Superczynski <jakubsuper1@gmail.com>
2021-05-24 01:02:17 +0200

    Wywolywanie zewnetrznych funkcji

--- ak2/lab4/steg.c
+++ ak2/lab4/steg.c
@@ -1,16 +1,18 @@
 #include "libbmp.h"
 
+bmp_pixel* code(bmp_pixel* pixels, char* text);
+
 int main ()
 {
     char file_name[260]="";
     printf("Podaj nazwe pliku: ");
     scanf("%s", file_name);
-    FILE *img_file = fopen(file_name, "rb");
-    bmp_header header;
-    bmp_header_read(&header, img_file);
-    fclose(img_file);
-    int x;
-    x = (int) header.biBitCount;
-    printf("%i", x);
+    char text[50]="";
+    printf("Podaj tekst do zakodowania: ");
+    scanf ("%s", text);
+    bmp_img img;
+    bmp_img_read(&img, file_name);
+    bmp_pixel* result = code(&img.img_pixels[0][0], &text[0]);
+    return 0;
 }
 
--- ak2/lab4/steg.s
+++ ak2/lab4/steg.s
@@ -1,14 +1,14 @@
-global steg
+global code
 
 section .text
 
-steg:
+code:
 
     push ebp
     mov ebp, esp
 
-    mov eax, [ebp+8]
-    add eax, 1
+    mov eax, [ebp+8] ;Poczatek pikseli
+    mov ebx, [ebp+12] ;Poczatek tekstu    
     
     pop ebp
     ret
