fae25c75cd5d742944a4c10ac26517a078e787f7
Jakub Superczynski <jakubsuper1@gmail.com>
2021-05-23 23:34:34 +0200

    Czytanie headera

--- ak2/lab4/steg.c
+++ ak2/lab4/steg.c
@@ -2,28 +2,15 @@
 
 int main ()
 {
-	bmp_img img;
-	bmp_img_init_df (&img, 512, 512);
-	
-	// Draw a checkerboard pattern:
-	for (size_t y = 0, x; y < 512; y++)
-	{
-		for (x = 0; x < 512; x++)
-		{
-			if ((y % 128 < 64 && x % 128 < 64) ||
-			    (y % 128 >= 64 && x % 128 >= 64))
-			{
-				bmp_pixel_init (&img.img_pixels[y][x], 250, 250, 250);
-			}
-			else
-			{
-				bmp_pixel_init (&img.img_pixels[y][x], 0, 0, 0);
-			}
-		}
-	}
-	
-	bmp_img_write (&img, "test.bmp");
-	bmp_img_free (&img);
-	return 0;
+    char file_name[260]="";
+    printf("Podaj nazwe pliku: ");
+    scanf("%s", file_name);
+    FILE *img_file = fopen(file_name, "rb");
+    bmp_header header;
+    bmp_header_read(&header, img_file);
+    fclose(img_file);
+    int x;
+    x = (int) header.biBitCount;
+    printf("%i", x);
 }
 
