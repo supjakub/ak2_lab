b078a8638c2f8cbdf0e81e8063d7923553e180d9
Jakub Superczynski <jakubsuper1@gmail.com>
2021-05-25 04:00:23 +0200

    Przesylanie rozmiarow

--- ak2/lab4/steg.c
+++ ak2/lab4/steg.c
@@ -1,6 +1,6 @@
 #include "libbmp.h"
 
-void code(bmp_pixel** pixels, char* text);
+void code(bmp_pixel** pixels, char* text, int width, int height);
 
 int main ()
 {
@@ -10,6 +10,13 @@ int main ()
     char text[50]="";
     printf("Podaj tekst do zakodowania: ");
     scanf ("%s", text);
+    int width, height;
+    bmp_header *header;
+    FILE *input = fopen(file_name, "rb");
+    bmp_header_read(header, input);
+    width = header->biWidth;
+    height = header->biHeight;
+    width = width * 3;
     bmp_img img;
     bmp_img_read(&img, file_name);
     code(img.img_pixels, &text[0]);
