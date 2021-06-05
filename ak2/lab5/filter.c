042dd07e24dcff46f8e89a11c5b38e5b716e9adf
Jakub Superczynski <jakubsuper1@gmail.com>
2021-06-06 00:39:35 +0200

    Wczytywanie pliku

---
+++ ak2/lab5/filter.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void filter(char *input_bits, long int width, long int height, unsigned short bpb, long int bpr, signed char *mask, char *output_bits);
+
+int main() {
+    char input_name[260]="";
+    printf("Podaj nazwe pliku: ");
+    scanf("%s", input_name);
+    FILE *input_file = fopen(input_name, "rb");
+    long int file_size;
+    fseek(input_file, 2, SEEK_SET);
+    fread(&file_size, sizeof(file_size),1,input_file);
+    long int pixels_offset;
+    fseek(input_file, 10, SEEK_SET);
+    fread(&pixels_offset, sizeof(pixels_offset),1,input_file);
+    long int biWidth;
+    fread(&biWidth, sizeof(biWidth),1,input_file);
+    fread(&biWidth, sizeof(biWidth),1,input_file);
+    long int biHeight;
+    fread(&biHeight, sizeof(biHeight),1,input_file);
+    unsigned short biBitCount;
+    fread(&biBitCount, sizeof(biBitCount),1,input_file);
+    fread(&biBitCount, sizeof(biBitCount),1,input_file);
+
+    long int bytes_count = biWidth * (long int) biBitCount;
+    bytes_count /= 8;
+    bytes_count *= biHeight;
+    unsigned char bits[bytes_count];
+    unsigned char header[pixels_offset];
+    fseek(input_file, 0, SEEK_SET);
+    fread(header, pixels_offset,1,input_file);
+    fread(bits, bytes_count,1,input_file);
+
+    long int footer_size = file_size - pixels_offset;
+    footer_size -= bytes_count;
+    unsigned char footer[footer_size];
+    fread(footer, footer_size,1,input_file);
+    fclose(input_file);
+
+    unsigned char output_bits[bytes_count];
+    signed char mask[] = {1, 0, -1, 0, 0, 0, -1, 0, 1};
+    filter(bits, biWidth, biHeight, biBitCount, (long int) bytes_count/biHeight, mask, output_bits);
+
+    FILE *output_file = fopen("result.bmp", "wb");
+    fwrite(header, pixels_offset,1,output_file);
+    fwrite(bits, bytes_count,1,output_file);
+    fwrite(footer, footer_size,1,output_file);
+    fclose(output_file);
+}
