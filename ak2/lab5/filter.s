71e8ebd11464c268f8c58ec76917661794042924
Jakub Superczynski <jakubsuper1@gmail.com>
2021-06-06 21:52:06 +0200

    Omijanie brzegow

--- ak2/lab5/filter.s
+++ ak2/lab5/filter.s
@@ -13,99 +13,56 @@ filter:
     push esi
     push edi
 
-    mov word [row], 1
-    mov word [column], 1
+    mov edx, [ebp+28]
     mov esi, [ebp+8]
     mov edi, [ebp+32]
-    mov edx, [ebp+28]
 
-    next_pixel:
-    mov cl, 0
-    mov bx, [ebp+12]
-    cmp bx, [column]
-    je next_row
-    cmp word [column], 1
-    je copy_byte
-    cmp word [row], 1
-    je copy_byte
-    mov bx, [ebp+16]
-    cmp word [row], bx
-    je copy_byte
-    next_byte:
-    mov ebx, 0
-    mov eax, 0
-    sub esi, [ebp+24]
-    dec esi
-    mov al, [esi]
-    imul byte [edx]
-    add ebx, eax
-    inc esi
-    mov al, [esi]
-    imul byte [edx+1]
-    add ebx, eax
-    inc esi
-    mov al, [esi]
-    imul byte [edx+2]
-    add ebx, eax
-    add esi, [ebp+24]
-    sub esi, 2
-    mov al, [esi]
-    imul byte [edx+3]
-    add ebx, eax
-    inc esi
-    mov al, [esi]
-    imul byte [edx+4]
-    add ebx, eax
-    inc esi
-    mov al, [esi]
-    imul byte [edx+5]
-    add ebx, eax
-    add esi, [ebp+24]
-    sub esi, 2
-    mov al, [esi]
-    imul byte [edx+6]
-    inc esi
-    mov al, [esi]
-    imul byte [edx+7]
-    add ebx, eax
-    inc esi
-    mov al, [esi]
-    imul byte [edx+8]
-    add ebx, eax
-    mov [edi], bl
-    sub esi, [ebp+24]
+    mov ecx, 0
+    first_row:
+    mov byte [edi], 255
     inc edi
-    inc cl
-    cmp cl, 3
-    jl next_byte
-    inc word [column]
-    jmp next_pixel
+    inc ecx
+    cmp ecx, [ebp+24]
+    jl first_row
 
+    mov word [row], 2
     next_row:
-    mov word [column], 0
+    mov ecx, 0
+    next_byte_left:
+    mov byte [edi], 255
+    inc edi
+    inc ecx
+    cmp ecx, [ebp+20]
+    jl next_byte_left
+
+    mov ecx, 6
+    next_byte:
+    mov byte [edi], 0
+    inc edi
+    inc ecx
+    cmp ecx, [ebp+24]
+    jl next_byte
+
+    mov ecx, 0
+    next_byte_right:
+    mov byte [edi], 255
+    inc edi
+    inc ecx
+    cmp ecx, [ebp+20]
+    jl next_byte_right
+
     inc word [row]
-    mov bx, [ebp+16]
-    cmp word [row], bx
-    jg end
-    copy_byte:
-    mov al, [esi]
-    mov [edi], al
+    mov cx, [row]
+    cmp cx, [ebp+16]
+    jl next_row
+
+    mov ecx, 0
+    last_row:
+    mov byte [edi], 255
     inc edi
-    inc esi
-    inc cl
-    cmp cl, 3
-    jl copy_byte
-    inc word [column]
-    jmp next_pixel
-    end:
-    copy_last:
-    mov al, [esi]
-    mov [edi], al
-    inc edi
-    inc esi
-    inc cl
-    cmp cl, [ebp+16]
-    jg copy_last
+    inc ecx
+    cmp ecx, [ebp+24]
+    jl last_row
 
     pop edi
     pop esi
